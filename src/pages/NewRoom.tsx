0fb8264ab0210de50c266d45672eefa7a8a733f6
Richard Ben <frichardben@yahoo.com.br>
2021-06-26 10:30:28 -0300

    feat: route room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FormEvent } from 'react';
 import { useAuth } from '../hooks/useAuth';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -14,7 +14,7 @@ import '../styles/auth.scss';
 
 export function NewRoom() {
     const { user } = useAuth();
-
+    const history = useHistory();
     const [newRoom, setNewRoom] = useState('');
 
      async function handleCreateRoom(event: FormEvent) {
@@ -29,6 +29,8 @@ export function NewRoom() {
             title: newRoom,
             authorId: user?.id,
         })
+
+        history.push(`/rooms/${fireBaseRoom.key}`)
     }
 
     return(
